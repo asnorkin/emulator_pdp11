7a1de57a77ed452a24dc7f6942fce5ff2f4e1910
artem <potapovart16@yandex.ru>
2017-01-20 14:56:30 +0300

    Image print

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,3 +1,8 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 #include "mainwidget.h"
 #include "ui_mainwidget.h"
 #include "pdp.h"
@@ -7,14 +12,36 @@
 #include <QImage>
 #include <QGraphicsPixmapItem>
 #include <QMessageBox>
+#include <QPainter>
+
+bool pause_pressed = false;
+bool started = false;
+bool finish = false;
+
+const std::string final = "HALT ";
+std::string file_name = "";
+int row;
+
+bool buffer_ready = false;
+unsigned char *buffer_ = NULL;
+
+int num_of_rows = 0;
+int breakpoint = -1;
+pdp pdp_instance;
 
 MainWidget::MainWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
-    QGraphicsScene* scene = new QGraphicsScene(10, 10, 261, 261, ui->graphicsView);
-    enable_false(ui);
+    pictureSet = false;
+
+    viewWidth = ui->graphicsView->width();
+    viewHeight = ui->graphicsView->height();
+
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+
     ui->pushButton_3->setEnabled(false);
     ui->pushButton_7->setEnabled(false);
     ui->pushButton_8->setEnabled(false);
@@ -23,19 +50,36 @@ MainWidget::MainWidget(QWidget *parent) :
 MainWidget::~MainWidget()
 {
     delete ui;
+    delete scene;
 }
+
+void MainWidget::paintEvent(QPaintEvent *event)
+{
+    if(buffer_ready == true) {
+        //////
+        struct stat buf;
+        stat("pause.rgba", &buf);
+
+        unsigned char* buffer = (unsigned char *)calloc(buf.st_size, sizeof(unsigned char));
+
+        int fd = open("pause.rgba", O_RDONLY);
+        pread(fd, buffer, buf.st_size, 0);
+        //close(fd);
+
+        QImage img(buffer, 256, 256, QImage::Format_RGBA8888);
+
+        //////
+        scene->clear();
+        scene->addPixmap(QPixmap::fromImage(img));
+        ui->graphicsView->update();
+    } else {
+        scene->clear();
+        ui->graphicsView->update();
+    }
+}
+
 //////////////////////////////////////////////////////
 
-bool pause_pressed = false;
-bool started = false;
-bool finish = false;
-const std::string final = "HALT ";
-std::string file_name = "";
-int row;
-
-int num_of_rows = 0;
-int breakpoint = -1;
-pdp pdp_instance;
 
 void free(pdp_status *instance) {
     delete[] (instance -> registers);
@@ -56,6 +100,10 @@ void set_text_in_labels(Ui::MainWidget *ui, pdp_status *instruction_content) {
     ui->label_14->setText(QString::number(instruction_content -> registers[5]));
     ui->label_15->setText(QString::number(instruction_content -> registers[6]));
     ui->label_16->setText(QString::number(instruction_content -> registers[7]));
+    ui->label_24->setText(QString::number(instruction_content -> N_flag));
+    ui->label_25->setText(QString::number(instruction_content -> Z_flag));
+    ui->label_26->setText(QString::number(instruction_content -> V_flag));
+    ui->label_27->setText(QString::number(instruction_content -> C_flag));
 }
 
 void enable_false(Ui::MainWidget *ui) {
@@ -73,10 +121,13 @@ void clear_labels(Ui::MainWidget *ui) {
     ui->label_14->clear();
     ui->label_15->clear();
     ui->label_16->clear();
+    ui->label_24->clear();
+    ui->label_25->clear();
+    ui->label_26->clear();
+    ui->label_27->clear();
 }
 
 void reset_common(Ui::MainWidget *ui) {
-    ui->graphicsView->setBackgroundBrush(Qt::white);
     clear_labels(ui);
     started = false;
     pause_pressed = false;
@@ -110,9 +161,18 @@ void MainWidget::on_pushButton_clicked() //start
             if(pause_pressed == true) break;
             instruction_content = pdp_instance.run_next_instruction();
             compare = instruction_content -> disasm_command;
+
+            if(buffer_ready == false) {
+                buffer_ = instruction_content -> VRAM;
+                buffer_ready = true;
+            } else {
+                //free(instruction_content -> VRAM);
+            }
+
             if(!final.compare(compare)) {
                 finish = true;
             }
+
             set_text_in_labels(ui, instruction_content);
             insert_row(ui);
             QTableWidgetItem *newItem_1 = new QTableWidgetItem();
@@ -121,6 +181,8 @@ void MainWidget::on_pushButton_clicked() //start
             newItem_2->setText(QString::number(instruction_content -> registers[7]));
             ui->tableWidget->setItem(num_of_rows-1, 0, newItem_1);
             ui->tableWidget->setItem(num_of_rows-1, 1, newItem_2);
+            //free(instruction_content -> registers);
+            free(instruction_content -> RAM);
             free(instruction_content);
         } while(final.compare(compare));
         if((pause_pressed != true) && (finish == true)) {
@@ -149,7 +211,7 @@ void MainWidget::on_pushButton_2_clicked() // step
         bool result = pdp_instance.load_program((char *)file_name.c_str());
         started = true;
         if(result != true) {
-            exit(-1); //TODO: Some other realization!
+            exit(-1);
         }
     }
 
@@ -158,6 +220,14 @@ void MainWidget::on_pushButton_2_clicked() // step
         if(!final.compare(instruction_content -> disasm_command)) {
             finish = true;
         }
+
+        if(buffer_ready == false) {
+            buffer_ = instruction_content -> VRAM;
+            buffer_ready = true;
+        } else {
+            //free(instruction_content -> VRAM);
+        }
+
         insert_row(ui);
         set_text_in_labels(ui, instruction_content);
         QTableWidgetItem *newItem_1 = new QTableWidgetItem();
@@ -182,6 +252,8 @@ void MainWidget::on_pushButton_5_clicked()
         QMessageBox::information(this,"Image Viewer","Error Displaying image");
         return;
     }
+    ui->lineEdit->clear();
+    file_name.clear();
     file_name = line_str.toStdString();
     ui->pushButton_5->setEnabled(false);
     ui->lineEdit->setEnabled(false);
@@ -193,12 +265,16 @@ void MainWidget::on_pushButton_5_clicked()
 void MainWidget::on_pushButton_6_clicked()
 {
     reset_common(ui);
+
     int i = num_of_rows;
     while(i > 0) {
         i--;
         ui->tableWidget->removeRow(0);
     }
     ui->pushButton_7->setEnabled(false);
+    buffer_ready = false;
+    //free(buffer_);
+    file_name.clear();
     num_of_rows = 0;
     breakpoint = -1;
 }
